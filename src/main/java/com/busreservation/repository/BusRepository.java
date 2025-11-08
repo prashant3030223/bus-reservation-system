95710b845be6499aa510082e36dd7704fe7998cb
Prashant Yadav <158310872+prashant3030223@users.noreply.github.com>
2025-11-08 03:12:50 +0000

    Added new features

--- src/main/java/com/busreservation/repository/BusRepository.java
+++
@@ -1,7 +0,0 @@
-package com.busreservation.repository;
-
-import com.busreservation.model.Bus;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BusRepository extends JpaRepository<Bus, Long> {
-}
--- src/main/java/com/busreservation/repository/ScheduleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.busreservation.repository;
-
-import com.busreservation.model.Schedule;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
-    List<Schedule> findBySourceAndDestination(String source, String destination);
-}
